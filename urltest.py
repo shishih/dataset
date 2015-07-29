fb641dc327aa5ed0a8c4bb8cbd83d3e31bbe28b0
shishih <agnesyao1110@gmail.com>
2015-07-29 16:57:02 -0400

    detect playlist

--- urltest.py
+++ urltest.py
@@ -2,11 +2,20 @@ import re
 import sys,os, urllib, urllib2, time, random, cookielib
 import threading
 
-linklist=['https://www.youtube.com/user/AynRandInstitute']
+file=open('youtubelist.txt')
+linklist=file.readlines()
+file.close()
+
+# linklist=['https://www.youtube.com/user/AynRandInstitute']
 for link in linklist:
     # to do :
     # if the url include playlist, then don't add videos in the end
-    source_url=link+'/videos'
+    pattern=re.compile(r'playlist')
+    match=pattern.search(link)
+    if match:
+        source_url=linkl
+    else:
+        source_url=link+'/videos'
     # 
     html=urllib2.urlopen(source_url)
     data=html.read()
