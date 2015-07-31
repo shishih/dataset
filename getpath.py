e324ed35f0203fba918d62e51b02c6059b5904c5
shishih <agnesyao1110@gmail.com>
2015-07-31 17:49:52 -0400

    finish list and txt

--- getpath.py
+++ getpath.py
@@ -3,11 +3,33 @@
 import os
 
 def getname(path):
-    name=[]
-    nameList=os.listdir(path)
-    for filename in nameList:
-        name.append(os.path.splitext(filename))
+    nameList=[]
+    initList=os.listdir(path)
+    for filename in initList:
+        nameList.append(os.path.splitext(filename))
+    return nameList
+def getSrt(nameList,file):
+    for name in nameList:
+        if name[1]=='.srt':
+            file.write(name[0]+name[1]+'\n')
+def getInput(nameList,file):
+    srtName=''
+    for name in nameList:
+        if name[1]=='.mp4':
+            file.write(name[0]+name[1]+',,'+srtName+'\n')
+        elif name[1]=='.srt':
+            srtName=name[0]+'txt'
 def main():
     # path='E:\users\yushiy\video\profiling'
-    path='F:\Research\Video\Dataset\coursera\popularity-001'
-    getname(path)
+    path='F:\Research\Video\Dataset\pathtest'
+    nameList=getname(path)
+    print nameList
+    file=open('srtlist.txt','a')
+    getSrt(nameList,file)
+    file.close()
+    file=open('inputList.txt','a')
+    getInput(nameList,file)
+    file.close()
+
+if __name__ == '__main__':
+    main()
--- srt2txt.py
+++ srt2txt.py
@@ -4,22 +4,32 @@ import re
 
 def trans(fileSrt,fileTxt):
     pattern=re.compile(r'^\d{1}.+\s')
+    patternRow=re.compile(r'^\d\s')
     data=fileSrt.readlines()
     for line in data:
         match=pattern.search(line)
         if match:
             pass
         else:
-            fileTxt.write(line)
+            matchRow=patternRow.search(line)
+            if matchRow:
+                pass
+            else:
+                fileTxt.write(line)
 
 def main():
-    srtFile='srttest.srt'
-    txtFile='txttest.txt'
-    fileSrt=open(srtFile,'a')
-    fileTxt=open(txtFile,'a')
-    trans(fileSrt,fileTxt)
-    fileSrt.close()
-    fileTxt.close()
+    path='F:\Research\Video\Dataset\pathtest'+'\\'
+    fileList=open('srtlist.txt')
+    data=fileList.readlines()
+    for line in data:
+        line=path+line.strip('\n')
+        srtFile=line
+        txtFile=line[:-3]+'txt'
+        fileSrt=open(srtFile)
+        fileTxt=open(txtFile,'a')
+        trans(fileSrt,fileTxt)
+        fileSrt.close()
+        fileTxt.close()
 
 if __name__ == '__main__':
     main()
